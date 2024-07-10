4f388752a8a11518882f17c2d707dd6d649498cc
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-11 00:32:21 +0200

    refactor: Update Verify page styles for better alignment and responsiveness

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -9,7 +9,7 @@ const Verify = () => {
   const { token } = useParams();
   const navigate = useNavigate();
   const [message, setMessage] = useState(
-    "Email is being verified, please wait..."
+    "Email is being verified, please wait"
   );
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -45,15 +45,15 @@ const Verify = () => {
 
   return (
     <div className="w-screen h-screen bg-habit-light-gray z-50 relative">
-      <div className="absolute w-[fit-content] p-2 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-center">
+      <div className="absolute w-[350px] md:w-[400px] p-2 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-center flex flex-col items-center justify-center">
         <Logo single={true} />
-        <p className="text-[10px] md:text-[14pxs] mt-4 flex gap-2">
-          <span className="text-lg text-[#ce8ef6]">{message}</span>
+        <p className="text-[10px] md:text-[14px] mt-4 flex gap-2">
+          <span className="text-sm md:text-lg text-[#ce8ef6]">{message}</span>
           {loading && (
             <ThreeDots
               visible={true}
-              height="30"
-              width="30"
+              height="20"
+              width="20"
               color="#CA87F4"
               radius="8"
               ariaLabel="three-dots-loading"
@@ -64,7 +64,7 @@ const Verify = () => {
         </p>
         <button
           onClick={() => navigate("/")}
-          className="py-1 px-2 text-center bg-habit-light-purple text-white rounded-xl hover:bg-purple-300 mt-4"
+          className="text-[10px] md:text-[14px] py-1 px-2 text-center bg-habit-light-purple text-white rounded-xl hover:bg-purple-300 mt-10"
         >
           Go Home
         </button>
